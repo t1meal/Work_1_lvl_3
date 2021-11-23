1982b1810e11bdad52b29eddabc8a94ba5394339
Alex <t1meal@yandex.ru>
2021-11-23 23:09:54 +0300

    ДЗ к 1-му уроку

--- src/root/Main.java
+++ src/root/Main.java
@@ -15,7 +15,27 @@ public class Main {
     Box<Orange> orangeBox1 = new Box<>();
     Box<Apple> appleBox2 = new Box<>();
 
-    
+        appleBox1.addFruit(new Apple("Antonovka"));
+        appleBox1.addManyFruit(new Apple("Shafran"), 3);
+        appleBox2.addManyFruit(new Apple("Melba"), 7);
+
+
+        orangeBox1.addFruit(new Orange("Pomelo"));
+        orangeBox1.addManyFruit(new Orange("Mandarin"), 5);
+
+        System.out.println(appleBox1.getFullWeight());
+        System.out.println(orangeBox1.getFullWeight());
+
+        System.out.println(appleBox1.compare(orangeBox1));
+
+
+        System.out.println(appleBox1.getFullWeight());
+        System.out.println(appleBox2.getFullWeight());
+
+        appleBox1.swiftTo(appleBox2);
+
+        System.out.println(appleBox1.getFullWeight());
+        System.out.println(appleBox2.getFullWeight());
     }
 
     public static void change (Integer[] arr, int i, int j){
